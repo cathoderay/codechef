d10238bdb47c9c428c526839b25b4e3feef4c04d
Ronald Kaiser <raios.catodicos@gmail.com>
2011-04-24 18:06:01 -0300

    solution for internet media type problem

---
+++ easy/internet_media_types.pl
@@ -0,0 +1,29 @@
+#
+# Problem: Internet Media Type
+# URL: http://www.codechef.com/problems/MIME2
+#
+# Author: Ronald Kaiser
+# Email: raios dot catodicos at gmail dot com
+#
+# Observation: this problem was in the 24th Apr Cook-off competition
+#
+
+
+use feature qw(say);
+
+my ($n, $q) = split /\s+/, <>;
+my $table = {};
+for (1 .. $n ) {
+    my ($key, $val) = split /\s+/, <>;
+    $table->{$key} = $val;	
+}
+
+for (1 .. $q) {
+    chomp(my $line = <>);
+    if ($line =~ m/.*\.(?<ext>[a-z0-9]+)/ig) {
+	my $ext = $+{ext};
+        (exists $table->{$ext}) ? say "$table->{$ext}" : say "unknown";
+    } else {
+        say "unknown";
+    }
+}
