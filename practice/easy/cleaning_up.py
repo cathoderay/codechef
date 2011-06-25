aed0e4ddacda5c5d676065bf7085417a5fa848db
Ronald Kaiser <raios.catodicos@gmail.com>
2011-06-25 17:35:48 -0300

    refactoring

--- practice/easy/cleaning_up.py
+++ practice/easy/cleaning_up.py
@@ -10,8 +10,8 @@ __email__ = "raios dot catodicos at gmail dot com"
 t = int(raw_input())
 
 for i in range(t):
-    n, m = [int(i) for i in raw_input().split()]
-    done = set([int(i) for i in raw_input().split()])
+    n, m = map(int, raw_input().split())
+    done = set(map(int, raw_input().split()))
     all = set(range(1, n + 1))
     to_do = list(all.difference(done))
     to_do.sort()
@@ -23,9 +23,5 @@ for i in range(t):
        else:
            assistant.append(to_do[i])
 
-    for i in range(len(chef)):
-        print chef[i],
-    print ""
-    for i in range(len(assistant)):
-        print assistant[i],
-    print ""
+    print ' '.join(map(str, chef))
+    print ' '.join(map(str, assistant))
