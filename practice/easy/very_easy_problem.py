7e33a7ca6f0de7100f8b46e2edc747ef824587b7
Ronald Kaiser <raios.catodicos@gmail.com>
2013-11-25 10:46:13 -0200

    updating to run on python3

--- practice/easy/very_easy_problem.py
+++ practice/easy/very_easy_problem.py
@@ -22,13 +22,13 @@ def solve(n):
     s.reverse()
     return '+'.join(s)
 
-print "137=%s" % solve(137)
-print "1315=%s" % solve(1315)
-print "73=%s" % solve(73)
-print "136=%s" % solve(136)
-print "255=%s" % solve(255)
-print "1384=%s" % solve(1384)
-print "16385=%s" % solve(16385)
+print("137=%s" % solve(137))
+print("1315=%s" % solve(1315))
+print("73=%s" % solve(73))
+print("136=%s" % solve(136))
+print("255=%s" % solve(255))
+print("1384=%s" % solve(1384))
+print("16385=%s" % solve(16385))
 
 
 """
