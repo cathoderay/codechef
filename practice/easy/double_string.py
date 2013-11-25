7b0b2a124a25164b8bec0e2ca64356ade33e02cf
Ronald Kaiser <raios.catodicos@gmail.com>
2013-11-25 10:47:05 -0200

    adding first solution to Double Strings problem...

---
+++ practice/easy/double_string.py
@@ -0,0 +1,15 @@
+
+"""
+ Problem: Double strings
+ URL: http://www.codechef.com/problems/DOUBLE
+"""
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+max = lambda x: x if x % 2 == 0 else x - 1
+
+for i in range(int(input())):
+    x = int(input())
+    print(max(x))
