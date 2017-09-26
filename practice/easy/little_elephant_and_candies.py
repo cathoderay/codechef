ffb05e235aaa7378d2210f5ddbe0d4bedabcb0cd
Ronald Kaiser <raios.catodicos@gmail.com>
2017-09-26 01:02:11 -0300

    removing old solutions

--- practice/easy/little_elephant_and_candies.py
+++
@@ -1,7 +0,0 @@
-t = int(raw_input())
-for i in range(t):
-    n, c = map(int, raw_input().split())
-    if sum(map(int, raw_input().split())) <= c:
-        print("Yes")
-        continue
-    print("No")
