758be7f73faf573218fbca06ddebaaec5a4f81ca
Ronald Kaiser <raios.catodicos@gmail.com>
2018-11-27 19:53:55 +0100

    faster solution precalculating number of distinct prime factors

--- contests/november-lunchtime-2018/USF.py
+++ contests/november-lunchtime-2018/USF.py
@@ -7,6 +7,15 @@ __author__ = "Ronald Kaiser"
 __email__ = "raios dot catodicos at gmail dot com"
 
 
+# distinct factors using sieve of eratosthenes
+lim = 10**5
+p = [0]*(lim+3) 
+for i in range(2, lim+1):
+    if p[i] == 0:
+        for k in range(i, lim+1, i):
+            p[k] += 1
+
+
 for _ in range(int(input())):
     n = input() 
     ns = list(map(int, input().split()))
@@ -14,19 +23,13 @@ for _ in range(int(input())):
 
     vs = [0]*(maxN+3)
 
-    for i in ns: 
-        vs[i] += 1
+    for i in ns: vs[i] += 1
 
-    p = [0]*(maxN+3)
     maxi = 0
     for g in range(2, maxN+1):
         size = 0
-        prime = False
-        if p[g] == 0: prime = True
         for k in range(g, maxN+1, g):
             size += vs[k]
-            if prime: p[k] += 1
-        if prime: p[g] = 1
         if (p[g]*size) > maxi:
             maxi = p[g]*size
     print(maxi)
