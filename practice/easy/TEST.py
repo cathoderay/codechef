9d7f963d40ed5c0f0e1d865131237f8c6d561a45
Ronald Kaiser <raios.catodicos@gmail.com>
2018-11-28 02:19:23 +0100

    adding stupid TEST solution

---
+++ practice/easy/TEST.py
@@ -0,0 +1,15 @@
+"""
+  Url: https://www.codechef.com/problems/TEST
+"""
+
+import sys
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+n = input()
+while int(n) is not 42:
+    print(n)
+    n = input()
