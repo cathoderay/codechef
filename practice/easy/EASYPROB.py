c4abf6b8afb5ac438edf5d96d67b7b18cce9d191
Ronald Kaiser <raios.catodicos@gmail.com>
2018-11-28 19:25:44 +0100

    adding EASYPROB solution and removing old one

--- practice/easy/EASYPROB.py
+++ practice/easy/EASYPROB.py
@@ -1,38 +1,35 @@
 """
- Problem: A very easy problem
- URL: http://www.codechef.com/problems/EASYPROB
+  Url: https://www.codechef.com/problems/EASYPROB
 """
+
 
 __author__ = "Ronald Kaiser"
 __email__ = "raios dot catodicos at gmail dot com"
 
 
-def solve(n):
+def repr(n):
     b = list(bin(n)[2:])
     b.reverse()
     s = []
     for i, v in enumerate(b):
         if v == '1':
             if i == 0 or i == 2:
-                s.append("2(%d)" % i)
+                s.append('2(%d)' % i)
             elif i == 1:
                 s.append("2")
             else:
-                s.append("2(%s)" % solve(i))
+                s.append("2(%s)" % repr(i))
     s.reverse()
     return '+'.join(s)
 
-print("137=%s" % solve(137))
-print("1315=%s" % solve(1315))
-print("73=%s" % solve(73))
-print("136=%s" % solve(136))
-print("255=%s" % solve(255))
-print("1384=%s" % solve(1384))
-print("16385=%s" % solve(16385))
+
+ns = [137, 1315, 73, 136, 255, 1384, 16385]
+for n in ns:
+    print("%d=%s" % (n, repr(n)))
 
 
 """
-Output:
+solution:
 137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
 1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)
 73=2(2(2)+2)+2(2+2(0))+2(0)
