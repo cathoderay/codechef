9a365b66516c918319a841a4d12f7f81ccfcdff2
Ronald Kaiser <raios.catodicos@gmail.com>
2018-11-29 16:54:56 +0100

    refactoring RIGHTRI

--- practice/easy/RIGHTRI.py
+++ practice/easy/RIGHTRI.py
@@ -15,9 +15,7 @@ for _ in range(int(input())):
     a = distance(x1, y1, x2, y2)
     b = distance(x2, y2, x3, y3)
     c = distance(x3, y3, x1, y1) 
-    h = max(a, b, c)
-    c = [a, b, c]; c.remove(h)
-    if h == sum(c):
+    if a == b + c or b == a + c or c == a + b:
         count += 1
     
 print(count)
