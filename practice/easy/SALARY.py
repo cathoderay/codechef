31f18e8db55977a1565839f899d10619302e0e07
Ronald Kaiser <raios.catodicos@gmail.com>
2018-11-30 02:11:20 +0100

    removing unused import

--- practice/easy/SALARY.py
+++ practice/easy/SALARY.py
@@ -7,9 +7,6 @@ __author__ = "Ronald Kaiser"
 __email__ = "raios dot catodicos at gmail dot com"
 
 
-import operator
-
-
 for _ in range(int(input())):
     N = int(input())
     W = list(map(int, input().split()))
