1f0cadfaa5a2731b0baf18a1812357e27de474ec
Ronald Kaiser <raios.catodicos@gmail.com>
2018-12-03 19:05:41 +0100

    adding ANUDTC solution

---
+++ practice/easy/ANUDTC.py
@@ -0,0 +1,26 @@
+"""
+  Url: https://www.codechef.com/problems/ANUDTC
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+    
+
+for _ in range(int(input())):
+    N = int(input())
+
+    if 360 % N == 0 and N <= 360:
+        print("y", end = " ")
+    else:
+        print("n", end = " ")
+
+    if N > 360:
+        print("n", end = " ")
+    else:
+        print("y", end = " ")
+
+    if N <= 26:
+        print("y")
+    else:
+        print("n")
