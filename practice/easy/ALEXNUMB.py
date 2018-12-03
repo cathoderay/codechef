c81406a81ba06c14d0d09a840113d8be2e211c17
Ronald Kaiser <raios.catodicos@gmail.com>
2018-12-03 15:20:58 +0100

    adding ALEXNUMB solution

---
+++ practice/easy/ALEXNUMB.py
@@ -0,0 +1,16 @@
+"""
+  Url: https://www.codechef.com/problems/ALEXNUMB
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+    
+
+for _ in range(int(input())):
+    n = int(input())
+    a = input()
+
+    print((n-1)*(n)//2)
+
+
