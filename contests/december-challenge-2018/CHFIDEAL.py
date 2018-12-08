2742fdc7328cac70797f4c4fad8652ae4ce1687b
Ronald Kaiser <raios.catodicos@gmail.com>
2018-12-08 19:16:43 +0100

    adding CHFIDEAL solution

---
+++ contests/december-challenge-2018/CHFIDEAL.py
@@ -0,0 +1,11 @@
+"""
+  Url: https://www.codechef.com/problems/CHFIDEAL
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+print(3) 
+print(1 if int(input()) == 2 else 2)
