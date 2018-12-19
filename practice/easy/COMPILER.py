d2f07edd92507a4968ea6a588c80deafcce33444
Ronald Kaiser <raios.catodicos@gmail.com>
2018-12-19 21:31:27 +0100

    adding COMPILER solution

---
+++ practice/easy/COMPILER.py
@@ -0,0 +1,30 @@
+"""
+  Url: https://www.codechef.com/problems/COMPILER
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+import sys
+
+
+for _ in range(int(input())):
+    s = sys.stdin.readline()
+    
+    n = 0
+    count = 0
+    t = 0
+    for c in s:
+        if c == '<':
+            n += 1
+        elif c == '>':
+            if n > 0:
+                n -= 1
+                count += 2
+                if n == 0:
+                    t = count
+            else:
+                break
+    print(t)
