8701a7e8d9e5868f49de2058795bd225d784a4b0
Ronald Kaiser <raios.catodicos@gmail.com>
2018-12-28 15:24:45 +0100

    adding DISCHAR solution

---
+++ practice/easy/DISCHAR.py
@@ -0,0 +1,11 @@
+"""
+  Url: https://www.codechef.com/problems/DISCHAR
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+for _ in range(int(input())):
+    print(len(set(input())))
