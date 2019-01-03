6d44ada7733178ca6034d87a3a45567698f2f9d2
Ronald Kaiser <raios.catodicos@gmail.com>
2019-01-03 16:32:31 +0100

    adding ICPC16C solution

---
+++ practice/easy/ICPC16C.py
@@ -0,0 +1,11 @@
+"""
+  Url: https://www.codechef.com/problems/ICPC16C
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+
+
+for _ in range(int(input())):
+    print(int(input()) % 9 + 1) 
