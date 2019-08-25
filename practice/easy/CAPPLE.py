683c416fb8d58ba0e879eabd85900e29a4809b3f
Ronald Kaiser <raios.catodicos@gmail.com>
2019-08-25 17:58:19 +0200

    adding CAPPLE solution

---
+++ practice/easy/CAPPLE.py
@@ -0,0 +1,12 @@
+"""
+  Url: https://www.codechef.com/problems/CAPPLE
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+    
+
+for _ in range(int(input())):
+    _ = int(input())
+    print(len(set(map(int, input().split()))))
