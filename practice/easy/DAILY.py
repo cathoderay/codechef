30b259eee8b0baeded13ec13fd6893f94d9563f6
Ronald Kaiser <raios.catodicos@gmail.com>
2019-09-23 21:48:58 +0200

    little refactoring

--- practice/easy/DAILY.py
+++ practice/easy/DAILY.py
@@ -14,6 +14,6 @@ X, N = map(int, input().split())
 cars = [input() for _ in range(N)]
 
 gp = lambda x: list(range(4*x-3, 4*x+1))+[56-2*x, 55-2*x]
-cab = lambda a, b: f(a) // (f(b) * f(a-b)) if a >= b else 0
+cab = lambda a: f(a) // (f(X) * f(a-X)) if a >= X else 0
 zeros = lambda car, i: [car[p - 1] for p in gp(i)].count('0')
-print(sum([cab(zeros(car, i), X) for car in cars for i in range(1, 10)]))
+print(sum([cab(zeros(car, i)) for car in cars for i in range(1, 10)]))
