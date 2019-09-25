d2bd79ea70e65c7cc153f7f03589bba3f9813775
Ronald Kaiser <raios.catodicos@gmail.com>
2019-09-25 20:21:21 +0200

    adding ANUTHM solution

---
+++ practice/easy/ANUTHM.py
@@ -0,0 +1,12 @@
+"""
+  Url: https://www.codechef.com/problems/ANUTHM
+"""
+
+
+__author__ = "Ronald Kaiser"
+__email__ = "raios dot catodicos at gmail dot com"
+    
+
+for _ in range(int(input())):
+    print(sum(map(int, input().split())) - 1)
+
